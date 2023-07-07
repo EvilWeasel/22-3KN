c14e9bd82d43c9aa100fc148450ee9b9a8340ece
Evilweasel <38261180+EvilWeasel@users.noreply.github.com>
2023-07-07 14:33:17 +0200

    added example on how namespaces work

---
+++ ProjektStrukturieren/Models/Customer.cs
@@ -0,0 +1,18 @@
+namespace Models
+{
+
+    public class Customer
+    {
+        public string Name { get; set; } = String.Empty;
+        public string Surname { get; set; } = String.Empty;
+    }
+}
+
+namespace DebugModels
+{
+    public class Manager
+    {
+        public string Name { get; set; } = String.Empty;
+        public string Surname { get; set; } = String.Empty;
+    }
+}
