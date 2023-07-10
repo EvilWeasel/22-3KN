aa64cf9d8bfe2a92c8331ea1c135872ee50cba23
Evilweasel <38261180+EvilWeasel@users.noreply.github.com>
2023-07-10 15:10:04 +0200

    added static-site project with index.html and started work on style.css

--- ProjektStrukturieren/Program.cs
+++ ProjektStrukturieren/Program.cs
@@ -1,6 +1,6 @@
 ﻿using Models;
 Customer customer1 = new Customer() { Name = "max", Surname = "muster" };
 Customer customer2 = new() { Name = "sofia", Surname = "muster" };
-Manager manager1 = new() { Name = "Heinz", Surname = "muster" };
+DebugModels.Manager manager1 = new() { Name = "Heinz", Surname = "muster" };
 
 Console.WriteLine(customer1.Name);
