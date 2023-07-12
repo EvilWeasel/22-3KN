a0913f4e3f8401fd788033f7dd3a5cffa7ee5d78
Evilweasel <38261180+EvilWeasel@users.noreply.github.com>
2023-07-12 14:27:09 +0200

    added code for animated clock; added example for calling API's in JS

--- Personal_Homepage/script.js
+++ Personal_Homepage/script.js
@@ -1,7 +1,29 @@
-console.log("Buxtehude");
+// Auf die Uhr im HTML zugreifen 
+//   und in einer Variablen speichern
+const clockElem = document.getElementById('clock');
+// Aktuelle Uhrzeit ermitteln => Funktion
+function getTime() {
+    let date = new Date();
+    let time = date.toLocaleTimeString();
+    // clockElem.innerText => Gilt für alle Formelemente
+    // clockElem.innerHTML => Alles was kein Formelement ist
+    clockElem.innerHTML = time;
+}
+// LOOP: Ermittel die Uhrzeit
+//   und setzen den Textwert der Uhr auf die aktuelle Zeit
+setInterval(getTime, 1000);
+// setInterval(() => {
+//     let date = new Date();
+//     let time = date.toLocaleTimeString();
+//     clockElem.innerHTML = time;
+// }, 1000);
 
-
-var clock = document.getElementById('clock');
-
-
-clock.innerHTML = "Buxtehude";
+const jokeElem = document.getElementById('joke');
+// C# => List<Person> persons = new List<Person>();
+fetch('https://api.chucknorris.io/jokes/random?category=dev')
+    .then(httpResponse => {
+        return httpResponse.json();
+    })
+    .then(joke => {
+        jokeElem.innerHTML = joke.value;
+    });
